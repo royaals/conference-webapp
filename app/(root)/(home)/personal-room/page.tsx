83fae321858a81f15d4e846a95659b85013f1f52
Royal <royal.21ai040@sode-edu.in>
2024-06-02 20:34:04 +0530

    add meeting page

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,34 +1,13 @@
-"use client";
 
 
-const Table = ({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) => {
-  return (
-    <div className="flex flex-col items-start gap-2 xl:flex-row">
-      <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
-        {title}:
-      </h1>
-      <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
-        {description}
-      </h1>
-    </div>
-  );
-};
-
-const PersonalRoom = () => {
-
-
-
+const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-     personal-room
+      <h1 className="text-3xl font-bold">Personal room</h1>
+
+      
     </section>
   );
 };
 
-export default PersonalRoom;
+export default PreviousPage;
