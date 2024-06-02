05080ede31b7d24ed71bfe604eb37fe2776aef14
Royal <royal.21ai040@sode-edu.in>
2024-06-02 20:30:39 +0530

    add meeting page

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,18 @@
+
+'use client';
+
+
+
+const MeetingTypeList = () => {
+
+
+
+
+  return (
+    <section className="flex flex-col gap-10 text-white">
+      meeting-type-list
+    </section>
+  );
+};
+
+export default MeetingTypeList;
